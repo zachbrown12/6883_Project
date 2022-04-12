21042b6f81291e706b450493e3098b24c75e608c
Zach Brown <zachmb22@gmail.com>
2022-04-12 19:34:44 -0400

    Add Beneficiary is working now, Added Cards to Actions

--- client/src/components/actions.js
+++ client/src/components/actions.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
-
+import InputGroup from 'react-bootstrap/InputGroup'
+import Form from 'react-bootstrap/Form'
 
 class Actions extends Component {
     constructor(props) {
@@ -14,20 +15,32 @@ class Actions extends Component {
     render() {
 
         return (
-            <li>{this.renderRows()}</li>
+            <div>
+                <h2>Beneficiaries</h2>
+                {this.renderRows()}
+            </div>
         )
     }
 
     renderRows() {
 
-    return this.props.beneficiaries.map((beneficiary) => {    
+    return this.props.beneficiaries.map((beneficiary, index) => {   
         return (
-            <div className="actions">
-                <h3>Choose to Give Assets</h3>
-                <div className = "bullets">
-                    <Button>
-                        Hi
-                    </Button>
+            <div className="card-ac">
+                <div key={index} class="card text-dark bg-light mb-3">
+                    Name: {beneficiary.name}<br></br>
+                    Address: {beneficiary.ben_address}<br></br>
+                    Payout: {beneficiary.payout}<br></br><br></br>
+                    <b>Choose to Give Assets</b>
+                    <div className = "bullets">
+                        <InputGroup className="mb-3">
+                            <Button variant="outline-secondary" id="button-addon1">
+                                Submit
+                            </Button>
+                            <Form.Control aria-label="Amount (to the nearest dollar)" />
+                            <InputGroup.Text>Wei</InputGroup.Text>
+                        </InputGroup>
+                    </div>
                 </div>
             </div>
         );
@@ -35,5 +48,7 @@ class Actions extends Component {
     }
 
 }
+
+
 
 export default Actions
