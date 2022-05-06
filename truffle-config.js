d5c1d398d5037ebba18f23686fe8cdc547534618
zachbrown12 <zachmb22@gmail.com>
2022-05-06 11:27:45 -0400

    Successfully migrated to CCN

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,18 @@
-var HDWalletProvider = require("truffle-hdwallet-provider")
+
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const privateKey= require('./secrets.json').privateKey;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  //contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8765,            // Standard Huygen port (default: none)
+      network_id: "*",   
+      //port: 8545 used this when using rinkeyby
     },
     rinkeby: {
       provider: function() { 
@@ -19,6 +24,27 @@ module.exports = {
       gas: 4500000,
       gasPrice: 10000000000,
     },
+    huygens: { 
+      networkCheckTimeout: 10000,
+      provider: () => new HDWalletProvider([privateKey], `https://huygens.computecoin.network`),
+      network_id: 828,
+      confirmations: 10,
+      timeoutBlocks: 20,
+      skipDryRun: true
+  },
+    huygens_dev: {
+      provider: () => new HDWalletProvider(
+        {
+          privateKeys: [privateKey], 
+          providerOrUrl: `https://test-huygens.computecoin.info`,
+          chainID: 3, 
+        }
+          ),
+      network_id: 828,
+      confirmations: 10,
+      timeoutBlocks: 20,
+      skipDryRun: true
+  },
   },
     compilers: {
       solc: {
